3e195eefa6f98e915a2a384fe486449247416d97
inparkha <inha2847@gmail.com>
2024-10-13 22:11:47 +0900

    반복문 실습

--- day07/day07-1.c
+++ day07/day07-1.c
@@ -6,12 +6,12 @@ int main()
 	scanf_s("%d", &N);
 
 	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N-i-1; j++)
+		for (int j = 0; j < N - i - 1; j++)
 		{
 			printf(" ");
 		}
-		
-		for(int k = 0; k<i*2+1; k++)
+
+		for (int k = 0; k < i * 2 + 1; k++)
 		{
 			printf("*");
 		}
